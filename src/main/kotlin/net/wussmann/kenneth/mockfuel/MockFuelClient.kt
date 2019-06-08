00d702d13b64aab884fb5fb750bcd02e3c004feb
Kenneth Wußmann <kenneth@wussmann.net>
2019-06-08 18:48:58 +0200

    Remove coroutines

--- src/main/kotlin/net/wussmann/kenneth/mockfuel/MockFuelClient.kt
+++ src/main/kotlin/net/wussmann/kenneth/mockfuel/MockFuelClient.kt
@@ -3,8 +3,6 @@ package net.wussmann.kenneth.mockfuel
 import com.github.kittinunf.fuel.core.Client
 import com.github.kittinunf.fuel.core.Request
 import com.github.kittinunf.fuel.core.Response
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
 import net.wussmann.kenneth.mockfuel.data.MockRequestMatcher
 
 /**
@@ -15,11 +13,9 @@ class MockFuelClient(private val mockFuelStore: MockFuelStore) : Client {
     override fun executeRequest(request: Request): Response {
         val response = mockFuelStore.findResponse(request)
         mockFuelStore.recordedRequests.add(MockRequestMatcher.from(request))
-        return if (response.delay > 0) {
-            runBlocking {
-                delay(response.delay)
-                response.toResponse(request.url)
-            }
-        } else response.toResponse(request.url)
+        if (response.delay > 0) {
+            Thread.sleep(response.delay)
+        }
+        return response.toResponse(request.url)
     }
 }
