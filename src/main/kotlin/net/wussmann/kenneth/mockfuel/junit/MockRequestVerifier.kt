a23d7986c594e7ab6ba447d3472d649c34088eb2
Kenneth Wußmann <kenneth@wussmann.net>
2019-06-23 18:27:19 +0200

    Use else instead of return

--- src/main/kotlin/net/wussmann/kenneth/mockfuel/junit/MockRequestVerifier.kt
+++ src/main/kotlin/net/wussmann/kenneth/mockfuel/junit/MockRequestVerifier.kt
@@ -58,11 +58,9 @@ class MockRequestVerifier(@Suppress("MemberVisibilityCanBePrivate") val request:
         }
     }
 
-    fun assertQueryParam(expectedKey: String) {
-        if (request.queryParams == null) {
-            assertAnyQueryParam()
-            return
-        }
+    fun assertQueryParam(expectedKey: String) = if (request.queryParams == null) {
+        assertAnyQueryParam()
+    } else {
         assertFalse(
             request.queryParams.get(expectedKey).isEmpty(),
             """Expected request query parameter <"$expectedKey"> to be present but was not found."""
